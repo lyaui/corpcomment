66c31d9b51a9fa6ac5d24e69ccadcb391be406dd
Iris Huang <lyaui@outloook.com>
2024-01-17 23:49:38 +0800

    feat: form text counter

--- src/components/FeedbackForm.tsx
+++ src/components/FeedbackForm.tsx
@@ -1,19 +1,32 @@
-import React from 'react';
+import { useState, type ChangeEvent } from 'react';
+import { MAX_CHARACTERS } from '../lib/constants';
 
 function FeedbackForm() {
+  const [text, setText] = useState('');
+
+  const charCnt = MAX_CHARACTERS - text.length;
+
+  const handleChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
+    const newText = event.target.value;
+    if (newText.length >= MAX_CHARACTERS) return;
+    setText(newText);
+  };
+
   return (
     <form className='form'>
       <textarea
-        name=''
+        value={text}
+        onChange={handleChange}
         id='feedback-textarea'
         placeholder=''
+        maxLength={MAX_CHARACTERS}
         spellCheck={false}
       />
       <label htmlFor='feedback-textarea'>
         Enter your feedback here, remember to #hashtag the company
       </label>
       <div>
-        <p className='u-italic'>150</p>
+        <p className='u-italic'>{charCnt}</p>
         <button>
           <span>Submit</span>
         </button>
